a124f3259df9228d5a724fc1dca002d926ab53d9
Nanuk Krinner <nkrinner@suse.de>
2013-09-11 14:36:11 +0200

    Bug 830265 make help link point to suse docs
    Added a dashboard with links to the installed SUSE manuals and the
    upstream documentation. Replaced the old "Help" link with a link to
    the help page. The links are broken untill ibs sr#28723 is accepted
    
    Mark the susedoc dashboard as a module
    
    Added dashboard declaration
    
    Removed Deployment guide from the help page

--- openstack_dashboard/settings.py
+++ openstack_dashboard/settings.py
@@ -54,7 +54,7 @@ STATIC_URL = '/static/'
 ROOT_URLCONF = 'openstack_dashboard.urls'
 
 HORIZON_CONFIG = {
-    'dashboards': ('project', 'admin', 'settings',),
+    'dashboards': ('project', 'admin', 'settings', 'susedoc',),
     'default_dashboard': 'project',
     'user_home': 'openstack_dashboard.views.get_user_home',
     'ajax_queue_limit': 10,
@@ -139,6 +139,7 @@ INSTALLED_APPS = (
     'openstack_dashboard.dashboards.project',
     'openstack_dashboard.dashboards.admin',
     'openstack_dashboard.dashboards.settings',
+    'openstack_dashboard.dashboards.susedoc',
     'openstack_auth',
 )
 
